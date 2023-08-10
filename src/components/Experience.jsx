da0fdc56608b99afc068984cb9f076405c91466e
Vinuja piumjith <vinujapiumjith@gmail.com>
2023-08-11 00:44:41 +0530

    update portfolio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -155,7 +155,7 @@ const Experience = () => {
             src: restapi,
             title: "Rest API",
             style: 'shadow-emerald-500',
-            data: 'zoom-out-right'
+            data: 'zoom-out-left'
         }
     ]
 
